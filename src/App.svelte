81d0cf28d4482d421b530bc30aebc9d602558e30
Alex Kräuchi <al@soup.one>
2023-10-27 13:03:05 +0200

    Implement animation & dynamic text

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,66 @@
 <script lang="ts">
+  import { colors, textCollection } from "./data";
   // TODO replace favicon
-  type Compliment = {
-    text: string;
-    maxLength: number;
-  };
-  // TODO add more colors like lightblue
-  let color: string = "#f4d03f";
+  let color: string;
+  let texts: string[];
 
-  // TODO get maxLength from data
+  async function infiniteLoop() {
+    texts = textCollection[getRandomIndex(textCollection)];
+    color = colors[getRandomIndex(colors)];
+
+    const longest = texts.reduce((a, b) => (a.length > b.length ? a : b));
+
+    await sleep(1000 * (longest.length + 2));
+
+    infiniteLoop();
+  }
+
+  function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  function getRandomIndex(array: any[]): number {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  infiniteLoop();
 </script>
 
 <main style="--color: {color};">
-  <div class="words">
-    <span>J</span>
-    <span>A</span>
-    <span>N</span>
-    <span>I</span>
-    <span>S</span>
-  </div>
-
-  <div class="words">
-    <span>H</span>
-    <span>Ö</span>
-    <span>R</span>
-    <span>T</span>
-  </div>
-
-  <div class="words">
-    <span>G</span>
-    <span>U</span>
-    <span>T</span>
-    <span>&nbsp;</span>
-    <span>Z</span>
-    <span>U</span>
-  </div>
-  <div class="words">
-    <span>G</span>
-    <span>U</span>
-    <span>T</span>
-    <span>&nbsp;</span>
-    <span>Z</span>
-    <span>U</span>
-  </div>
+  {#key JSON.stringify(texts)}
+    {#each texts as text}
+      <div class="text">
+        {#each text as character}
+          <span>{@html character.replace(" ", "&nbsp;")}</span>
+        {/each}
+      </div>
+    {/each}
+  {/key}
 </main>
 
 <style lang="scss">
   main {
     font-family: "Archivo Black", sans-serif;
-    background: var(--color);
+    background-color: var(--color);
     display: flex;
     flex-direction: column;
     height: 100vh;
     height: 100svh;
     justify-content: center;
     align-items: center;
+    transition: background-color 1s ease, color 1s ease;
+    color: var(--color);
   }
 
-  .words {
-    color: var(--color);
+  .text {
     font-size: 0;
     line-height: 1.5;
   }
 
-  .words span {
+  .text span {
     font-size: 5rem;
     display: inline-block;
-    animation: move 3s ease-in-out infinite;
+    animation: move 3s ease-in-out 2;
   }
 
   @keyframes move {
@@ -80,11 +75,17 @@
     }
   }
 
-  .words span {
-    @for $i from 2 through 11 {
+  .text span {
+    @for $i from 2 through 14 {
       &:nth-child(#{$i}) {
         animation-delay: 0.5s + ($i - 2) * 0.5s;
       }
+    }
+  }
+
+  @media (max-width: 380px) {
+    .text span {
+      font-size: 3.5;
     }
   }
 </style>
